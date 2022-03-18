be144c93fec3527237ae9c37be7b8fd473fc1569
lane <laneolson@gmail.com>
2022-03-18 12:54:45 -0700

    feat: add build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const path = require("path");
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "HypReactDashboard",
+      fileName: (format) => `HypReactDashboard.${format}.js`,
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "mapbox-gl"],
+      output: {
+        globals: {
+          react: "react",
+          "react-dom": "ReactDOM",
+          "mapbox-gl": "mapboxgl",
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
